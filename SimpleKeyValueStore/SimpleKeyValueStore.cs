8dde0bd7d3e0641f8500f114b25ef612dfcb5e0c
Robin <robin.kaiser@haevg-rz.de>
2020-07-14 14:24:44 +0200

    initial commmit

---
+++ SimpleKeyValueStore/SimpleKeyValueStore.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace SimpleKeyValueStore
+{
+    public class SimpleKeyValueStore
+    {
+        private IDictionary<string, string> dict;
+
+        private readonly string defaultPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\SimpleKeyValueStore\";
+
+        private Context context;
+
+
+        public SimpleKeyValueStore(Context ctx = null)
+        {
+            this.Setup(ctx);
+        }
+
+        public SimpleKeyValueStore(string name)
+        {
+            this.Setup(new Context() {Name = name});
+        }
+
+        private void Setup(Context ctx)
+        {
+            this.dict = new ConcurrentDictionary<string, string>();
+
+            if (ctx == null)
+            {
+                this.context = new Context()
+                {
+                    Name = "arbitrary",
+                    Path = null
+                };
+            }
+
+            if (this.context.Path == null)
+                this.context.Path = this.defaultPath;
+
+            if (!Directory.Exists(this.context.Path))
+                Directory.CreateDirectory(this.context.Path);
+
+            this.dict = this.LoadFromStore();
+        }
+
+        public void Set(string key, string value)
+        {
+            if (this.dict.ContainsKey(key))
+                this.dict[key] = value;
+            else
+                this.dict.Add(key, value);
+        }
+
+        public (string, bool) TryGet(string key)
+        {
+            if (this.dict.ContainsKey(key))
+                return (this.dict[key], true);
+
+            return (null, false);
+        }
+
+        public void Flush()
+        {
+            string file = this.context.Path + this.context.Name + ".bin";
+            File.WriteAllText(file, this.ToJson(this.dict));
+        }
+
+        private Dictionary<string, string> LoadFromStore()
+        {
+            var d = new Dictionary<string, string>();
+            string file = this.context.Path + this.context.Name + ".bin";
+            if (File.Exists(file))
+            {
+                var t = this.FromJson<Dictionary<string, string>>(File.ReadAllText(file));
+                if (t != null)
+                    d = t;
+            }
+
+            return d;
+        }
+
+        private string ToJson(object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+
+        private T FromJson<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+
+    public class Context
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestApp
+{
+    using SimpleKeyValueStore;
+
+    class Program
+    {
+        private static SimpleKeyValueStore kvs = new SimpleKeyValueStore();
+
+        static void Main(string[] args)
+        {
+            Benchmark(DoStuff, 10_000_000);
+
+            Console.ReadKey();
+        }
+
+        private static void DoStuff()
+        {
+
+            //kvs.Set("hello", "world 3");
+            //kvs.Flush();
+
+            kvs.TryGet("hello");
+
+            //(string message, bool ok) = kvs.TryGet("hello");
+            //if (ok)
+            //{
+            //    Console.WriteLine("got value: " + message);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("could not get value");
+            //}
+        }
+
+        private static void Benchmark(Action act, int interval)
+        {
+            GC.Collect();
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < interval; i++)
+            {
+                act.Invoke();
+            }
+            sw.Stop();
+            Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+        }
+    }
+}
